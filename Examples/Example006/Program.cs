500b0197001ae103d067f20f7ad05a758e1c9c2f
Aleks <sankov1999ca14@mail.ru>
2023-05-20 18:01:44 +0300

    Example006

---
+++ Examples/Example006/Program.cs
@@ -0,0 +1,15 @@
+﻿﻿int nun1 = 2;
+int nun2 = 3;
+int nun3 = 6; 
+int nun4 = 8;
+int nun5 = 4; 
+
+int max = nun1;
+
+if (max < nun2) max = nun2;
+if (max < nun3) max = nun3;
+if (max < nun4) max = nun4;
+if (max < nun5) max = nun5;
+
+Console.Write("max = ");
+Console.WriteLine(max);
