73b52d35ff232d03ec9af644fb3e264da1b3b7bd
Aleks <sankov1999ca14@mail.ru>
2023-05-20 19:55:03 +0300

    zadanie4

---
+++ Seminar1/Zadanie4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выход выдает все целые числа в промежутке от -N до N
+Console.WriteLine("Введите N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int n1 = -1*n ;
+
+while (n1<=n)
+{
+    Console.Write(n1+ ",");
+    n1 = n1+1 ;
+}
