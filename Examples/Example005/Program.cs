baedb28b8796edb7006c99d42c07bbcf2a78081a
Aleks <sankov1999ca14@mail.ru>
2023-05-20 18:00:09 +0300

    Example005

---
+++ Examples/Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿﻿Console.Write("ВВедите Ваше Имя ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "саша")
+{
+    Console.WriteLine("Ура, этоже Саша!");
+}
+else
+{ 
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
