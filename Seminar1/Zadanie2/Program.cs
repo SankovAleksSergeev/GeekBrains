f96cba678d254e6f13db957fe205fa6d9137555f
Aleks <sankov1999ca14@mail.ru>
2023-05-20 19:35:05 +0300

    zadanie2

---
+++ Seminar1/Zadanie2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу которая на вход от пользователя принимает два числа и проверяет является ли оно квадратом второго 
+Console.WriteLine("Введеите число один");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введеите число один");
+int b = Convert.ToInt32(Console.ReadLine()); 
+
+if (a == b*b)
+{
+    Console.Write("Да");
+}
+else 
+{
+    Console.Write("Нет");
+}
+
