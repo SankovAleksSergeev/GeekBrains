2a4ed12e217a9179c2feddb1ddb1ba72503a35e1
Aleks <sankov1999ca14@mail.ru>
2023-05-20 21:03:42 +0300

    Недоделанное д/з

---
+++ Seminar1/HomeworkSeminar1/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("Введите первое число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
+// {
+//     Console.Write("max = ");
+//     Console.Write(num1);
+// }
+// else
+// {
+//     Console.Write("max = ");
+//     Console.Write(num2);
+// }
+
+
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// int max = 0;
+
+// Console.WriteLine("Введите первое число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите третье число");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+
+// if (num1 > num2)
+// {
+//     max = num1;
+// }
+// else
+// {
+//     max = num2;
+// }
+
+// if (max > num3)
+// {
+//     Console.Write("max = ");
+//     Console.Write(max);
+// }
+// else
+// {
+//     max = num3;
+//     Console.Write("max = ");
+//     Console.Write(max);
+// }
+
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число");
+string num = Console.ReadLine();
+
+if (num = 2)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
