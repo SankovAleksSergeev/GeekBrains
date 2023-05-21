690b9d486d7496d49c5996d82e589b2fd1cf2d2a
Aleks <sankov1999ca14@mail.ru>
2023-05-21 14:32:03 +0300

    Lecture2

---
+++ Examples/Lecture2/Primer1/Program.cs
@@ -0,0 +1,25 @@
+﻿int Max (int arg1 , int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 635;
+int a2 = 745;
+int a3 = 455;
+int b1 = 623;
+int b2 = 4826;
+int b3 = 142;
+int c1 = 22212;
+int c2 = 848;
+int c3 = 869;
+
+// int max1 = Max (a1 , a2 , a3);       
+// int max2 = Max (b1 , b2 , b3);
+// int max3 = Max (c1 , c2 , c3);
+// int max = Max(max1, max2, max3);
+// Console.Write(max);                  Эту часть кода можно записать компактнее:
+
+int max = Max (Max(a1, a2, a3),Max (b1, b2, b3), Max(c1, c2, c3)); 
+Console.Write(max); 
---
+++ Examples/Lecture2/Primer4/Program.cs
@@ -0,0 +1,52 @@
+﻿void NapolnenieArray (int[] Collection)
+{
+    int Length = Collection.Length;
+    int count = 0;
+    while (count < Length)
+    {
+        Collection[count]= new Random().Next(0,10);
+        count++;
+    }    
+
+}
+
+void PrintArray (int[] Coll)
+
+{
+    int ind = Coll.Length;
+    int index = 0;
+    while(index < ind)
+    {
+        Console.WriteLine(Coll[index]);
+        index++;
+    }
+}
+
+int IndexOf (int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0; 
+    int num = -1;
+    while(index < count)
+    {
+        if (collection[index]==find)
+        {
+           num = index;
+           break; 
+        }
+
+        index++;
+    }
+    return num;
+}
+
+
+int [] array = new int [10];
+
+NapolnenieArray(array);
+PrintArray (array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
+
