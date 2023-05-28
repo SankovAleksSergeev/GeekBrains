a975305f8eae7da3ccf875730bb80f5ddc19e627
Aleks <sankov1999ca14@mail.ru>
2023-05-28 17:26:19 +0300

    Seminar3

---
+++ Seminar3/Zadanie2/Program.cs
@@ -0,0 +1,64 @@
+﻿//Обратная задача, написать программу которая при вводе числа от 1 до 4 выдает какие могут быть x и y 
+int Print (string Massange)
+{
+    Console.WriteLine(Massange);
+    string Peremen = Console.ReadLine();
+    int Peremen2 = Convert.ToInt32(Peremen);
+    return Peremen2;
+} 
+int x = Print("Введите номер координатной плоскости");
+
+switch (x)
+{
+    case 1:
+    {
+        Console.WriteLine("(x>0)&&(y>0)");
+        break;
+    }
+    case 2:
+    {
+        Console.WriteLine("(x<0)&&(y>0)");
+        break;
+    }
+    case 3:
+    {
+        Console.WriteLine("(x<0)&&(y<0)");
+        break;
+    }
+    case 4:
+    {
+        Console.WriteLine("(x>0)&&(y<0)");
+        break;
+    }
+    default:
+    {
+        Console.WriteLine("Ошибка. Неверное число");
+        break;
+    }
+}
+    
+// }
+// if (x==1)
+// {
+//     Console.WriteLine("(x>0)&&(y>0)");
+// }
+// else if (x==2)
+// {
+//      Console.WriteLine("(x<0)&&(y>0)");
+// }
+// else if (x==3)
+// {
+//      Console.WriteLine("(x<0)&&(y<0)");
+// }
+// else if (x==4)
+// {
+//      Console.WriteLine("(x>0)&&(y<0)");
+// }
+// else if (x==0)
+// {
+//      Console.WriteLine("Начальная точка (x==0)&&(y==0)");
+// }
+// else if ((x>4)&&(x<0))
+// {
+//      Console.WriteLine("Ошибка. Неверное число");
+// }
---
+++ Seminar3/Zadanie4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу которая на вход принимает число и выдает 
+// таблицу квадратных чисел 
+// Например 5 = 1, 4, 9, 16, 25
+//          2 = 1, 4
+
+int Print (string Massange)
+{
+    Console.WriteLine(Massange);
+    string Peremen = Console.ReadLine();
+    int Peremen2 = Convert.ToInt32(Peremen);
+    return Peremen2;
+
+}
+int num  = Print ("Введите число");
+int count = 1;
+while ( num> count)
+{
+    Console.Write($" {Math.Pow(count,2)},");
+    count= count +1;
+}
