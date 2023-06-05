e2628fedfcfe485e4fc7df8931b132a2657fab39
AleksandrSankov <sankov1999ca14@mail.ru>
2023-06-05 17:52:55 +0300

    IndependentWork5

--- Examples/Lecture4/Primer1/Program.cs
+++ Examples/Lecture4/Primer1/Program.cs
@@ -14,32 +14,32 @@
 //  }
 
 
-// void PrintArray (int [,]matr)
-// { 
-//     for (int i =0 ; i<matr.GetLength(0); i++)
-//     {
-//         for (int j =0; j<matr.GetLength(1); j++)
-//         {
-//             Console.Write($"{matr[i,j]} ");
-//         }
-//     Console.WriteLine();
-//     }
-// }
-// void FillArray (int [,] matr)
-// {
-//     for (int i =0 ; i<matr.GetLength(0); i++)
-//     {
-//         for (int j =0; j<matr.GetLength(1); j++)
-//         {
-//             matr[i,j]= new Random().Next(1,10);
-//         }
-//     }
-// }
-// int [,] matrix = new int [3,4];
-// PrintArray (matrix);
-// FillArray(matrix);
-// Console.WriteLine();
-// PrintArray (matrix);
+void PrintArray (int [,]matr)
+{ 
+    for (int i =0 ; i<matr.GetLength(0); i++)
+    {
+        for (int j =0; j<matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+void FillArray (int [,] matr)
+{
+    for (int i =0 ; i<matr.GetLength(0); i++)
+    {
+        for (int j =0; j<matr.GetLength(1); j++)
+        {
+            matr[i,j]= new Random().Next(1,10);
+        }
+    }
+}
+int [,] matrix = new int [3,4];
+PrintArray (matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray (matrix);
 
 //-----------------------------------------------------------------------------
 
@@ -120,12 +120,12 @@
 // Числа фибоначчи - это означает что каждое последующее число задается предыдущем 
 // F(n) = F(n-1)+F(n-2)
 
-double Fibonacci (int n) 
-{
-    if (n==1||n==2) return 1 ; 
-    else return Fibonacci(n-1)+ Fibonacci(n-2) ;
-}
-for (int i =1; i<40; i++)
-{
-    Console.WriteLine($"{i} = {Fibonacci(i)}");
-}
+// double Fibonacci (int n) 
+// {
+//     if (n==1||n==2) return 1 ; 
+//     else return Fibonacci(n-1)+ Fibonacci(n-2) ;
+// }
+// for (int i =1; i<40; i++)
+// {
+//     Console.WriteLine($"{i} = {Fibonacci(i)}");
+// }
